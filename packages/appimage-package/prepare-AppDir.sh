e3e834d3d730b1ebb2d65590fc0e49f23be5077b
alexander <116556921+al-kau@users.noreply.github.com>
2024-07-04 21:16:31 +0300

    AppImage package (#198)
    * Add 'AppImage' package template
    
    * Change 'publish.yml' workflow
    - 'AppImage' package was added to publish workflow
    
    * Fix 'publish.yml' workflow
    - added code that creates the package bin folder
    - changed tag version to commit version for github actions
    - added code that checks the AZURE secrets
    - fix appimage package
    
    * Fix 'publish.yml' workflow
    - assets uploading has been fixed
    
    * Change 'publish.yml' workflow
    - added condition that checks the AZURE secrets only on windows

---
+++ packages/appimage-package/prepare-AppDir.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+SCRIPT_DIR="$(dirname -- "$(readlink -f -- "${0}")")"
+BIN_DIR="$(readlink -f -- "${1}")"
+
+echo "script dir: $SCRIPT_DIR"
+echo "bin dir: $BIN_DIR"
+
+mkdir -p "$SCRIPT_DIR/AppDir/usr/bin/"
+cp -a "$BIN_DIR/." "$SCRIPT_DIR/AppDir/usr/bin/"
+
+chmod a+x "$SCRIPT_DIR/AppDir/AppRun"
+chmod a+x "$SCRIPT_DIR/AppDir/usr/bin/eppie-console"
